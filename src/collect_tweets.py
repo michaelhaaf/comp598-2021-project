16eb9b78f90960b77d886ded0a45dbca489afbf1
michaelhaaf <michael.haaf@gmail.com>
2021-11-30 14:58:55 -0500

    fixed collect_tweets, 1000 tweets from 3 day window collected

--- src/collect_tweets.py
+++ src/collect_tweets.py
@@ -4,68 +4,67 @@ import pandas as pd
 import argparse
 import json
 import os, sys
+from datetime import datetime, timedelta
 
 from dotenv import load_dotenv
 from pathlib import Path
 
-NUM_POSTS = 1000
+
+NUM_TWEETS = 1000
+API_LIMIT = 50 # reduced by factor of two, just in case
+WINDOW_SIZE = 3 # in days
 
 load_dotenv()
 API_KEY = os.environ.get('API_KEY')
 API_SECRET = os.environ.get('API_SECRET')
 ACCESS_TOKEN = os.environ.get('ACCESS_TOKEN')
 ACCESS_TOKEN_SECRET = os.environ.get('ACCESS_TOKEN_SECRET')
-
-def api_setup():
-    auth = tw.OAuthHandler(API_KEY, API_SECRET)
-    auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
-    api = tw.API(auth, wait_on_rate_limit=True)
-    print(api.verify_credentials().screen_name)
-    return api
-
-    # eg search_query = "#covid19 -filter:retweets"
-def twitter_fetch(api, query):
-    print(f'Fetching {query} using api object {api}')
-    tweets = tw.Cursor(api.search_tweets,
-            q=query,
-            lang="en").items(1000)
-    return tweets
+BEARER_TOKEN = os.environ.get('BEARER_TOKEN')
 
 
-def tweets_to_df(api, tweets):
-    df = pd.DataFrame()
+def client_setup():
+    client = tw.Client(
+            bearer_token=BEARER_TOKEN,
+            consumer_key=API_KEY,
+            consumer_secret=API_SECRET,
+            access_token=ACCESS_TOKEN,
+            access_token_secret=ACCESS_TOKEN_SECRET,
+            wait_on_rate_limit=True)
+    return client
 
-    for tweet in tweets:
-        hashtags = []
-        try:
-            for hashtag in tweet.entities["hashtags"]:
-                hashtags.append(hashtag["text"])
-            text = api.get_status(id=tweet.id, tweet_mode='extended').full_text
-        except:
-            pass
 
-        tweets_df = tweets_df.append(pd.DataFrame({'user_name': tweet.user.name,
-                                                   'user_location': tweet.user.location,
-                                                   'user_description': tweet.user.description,
-                                                   'user_verified': tweet.user.verified,
-                                                   'date': tweet.created_at,
-                                                   'text': text,
-                                                   'hashtags': [hashtags if hashtags else None],
-                                                   'source': tweet.source}))
-        tweets_df = tweets_df.reset_index(drop=True)
+def twitter_fetch(client, query):
+    print(f'Fetching {query} using api object {client}')
+    tweet_search = tw.Paginator(client.search_recent_tweets,
+            query=query,
+            start_time=datetime.now() - timedelta(days=WINDOW_SIZE),
+            max_results=API_LIMIT
+            ).flatten(limit=NUM_TWEETS)
+    return tweet_search
 
+
+def tweets_to_dict(tweets):
+    print("Collecting tweets. Might be paginating; usually takes less than a minute...")
+    return {tweet.id: tweet.text for tweet in tweets}
+
+
+def build_query():
+    vaccine_brands = ["pfizer", "moderna", "astrazeneca"]
+    key_words = ["covid", "vaccination"]
+    params = " OR ".join(vaccine_brands + key_words)
+    return f"({params}) lang:en -is:retweet"
 
 def main(args):
-    api = api_setup()
-    print(api)
-    search_query = "#covid19 -filter:retweets"
-    tweets = twitter_fetch(api, search_query)
-    df = tweets_to_df(api, tweets)
-    df.to_json(args.outputFile, indent=4) 
+    client = client_setup()
+    search_query = build_query()
+    tweets = twitter_fetch(client, search_query)
+    output_dict = tweets_to_dict(tweets)
+    with open(args.outputFile, "w") as fh:
+        json.dump(output_dict, fh, indent=4)
 
 
 ## Usage
-# python3 collect_newest.py -o <output_file> -s <subreddit>
+# python3 collect_newest.py -o <output_file> 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='collects 1000 tweets and stores them in a named file')
     parser.add_argument('-o',
