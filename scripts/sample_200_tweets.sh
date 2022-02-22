e20579ba4225a1daf1e90b2c2afebd4d2aaaef0e
michaelhaaf <michael.haaf@gmail.com>
2022-02-22 14:52:22 -0500

    end of semester

--- scripts/sample_200_tweets.sh
+++ scripts/sample_200_tweets.sh
@@ -5,4 +5,4 @@
 # Removes the first and last json braces, as well as the comma-less last line. Shuffles then selects 200. Adds json braces again, then prints to stdout
 
 input=$1
-cat "$input" | sed '1,1d' | sed '$d' | sed '$d' | shuf -n 200 | sed '$s/,$//' | cat <(echo "{") - <(echo "}") 
+sed '1,1d' "$input" | sed '$d' | sed '$d' | shuf -n 200 | sed '$s/,$//' | cat <(echo "{") - <(echo "}") 
